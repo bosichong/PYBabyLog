2b7c5fe84a41ad21d8736dc4bba630a2df3f8c49
J_sky <bosichong@qq.com>
2019-09-26 22:38:29 +0800

    终端添加数据功能完成

---
+++ DbHelp.py
@@ -0,0 +1,45 @@
+# -*- coding:utf-8 -*-
+
+
+'''
+
+数据库连接封装类
+
+'''
+import pymysql
+import datetime
+
+class Db():
+    def __init__(self, host='localhost', port=3306, db='mybaby', user='mybaby', passwd='mybaby20170606', charset='utf8'):
+        # 建立连接
+        self.conn = pymysql.connect(host=host, port=port, db=db, user=user, passwd=passwd, charset=charset)
+        # 创建游标，操作设置为字典类型
+        self.cur = self.conn.cursor(cursor = pymysql.cursors.DictCursor)
+
+    def __enter__(self):
+        # 返回游标
+        return self.cur
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # 提交数据库并执行
+        self.conn.commit()
+        # 关闭游标
+        self.cur.close()
+        # 关闭数据库连接
+        self.conn.close()
+
+
+if __name__ == '__main__':
+    with Db() as db:
+        try:
+            # 执行sql语句
+            db.execute('SELECT * FROM bb_blog LIMIT 10')
+            print(db)
+            for i in db:
+                print(i['id'])
+        except:
+            # 如果发生错误则回滚
+            db.rollback()
+            print("数据插入未成功！")
+
+
---
+++ Insert.py
@@ -0,0 +1,91 @@
+# -*- coding:utf-8 -*-
+'''
+
+终端插入日志记录，身高，体重
+
+'''
+
+import datetime
+import argparse
+
+from DbHelp import *
+
+
+def insertBlog(first='', language='', cognitive='', blog=''):
+    '''
+    添加日志等数据
+    blog 为必填项，其他可以为空
+
+    :param first: 第一次
+    :param language: 学会的语言
+    :param cognitive: 认知
+    :param blog: 日志
+    :return: bool
+    '''
+    create_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    update_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    baby_id = 1
+    user_id = 1
+    sql = "insert into bb_blog(first,language,cognitive,blog,create_time,update_time,baby_id,user_id) values('{0}','{1}','{2}','{3}','{4}','{5}',{6},{7})" \
+        .format(first, language, cognitive, blog, create_time, update_time, baby_id, user_id)
+    # print(sql)
+    with Db() as db:
+        try:
+            # 执行sql语句
+            db.execute(sql)
+            # 提交到数据库执行
+            print("数据添加成功！")
+        except:
+            # 如果发生错误则回滚
+            # db.rollback()
+            print("数据插添加失败！！")
+
+
+def insertHeightWeight(height,weight):
+    '''
+    添加孩子身高体重数据
+    :param height: int 身高厘米
+    :param weight: float 体重 公斤，保存小数点后一位即可
+    :return: bool
+    '''
+    create_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    baby_id = 1
+    sql = "insert into bb_healthy(height, weight, create_time, baby_id) values('{0}','{1}','{2}','{3}')" \
+        .format(height, weight, create_time, baby_id)
+    # print(sql)
+    with Db() as db:
+        try:
+            # 执行sql语句
+            db.execute(sql)
+            # 提交到数据库执行
+            print("数据添加成功！")
+        except:
+            # 如果发生错误则回滚
+            # db.rollback()
+            print("数据插添加失败！！")
+
+
+def main():
+
+    parser = argparse.ArgumentParser(prog="Insert", description="添加babyblog数据，添加身高体重数据。")
+    parser.add_argument("-f", "--first", help="请输入first数据", default='')
+    parser.add_argument("-l", "--language", help="请输入language数据", default='')
+    parser.add_argument("-c", "--cognitive", help="请输入cognitive数据", default='')
+    parser.add_argument("-b", "--blog", help="请输入blog数据,此数据必填！", )
+    parser.add_argument("-ht", "--height", help="填写身高厘米,身高体重必须同时填写。",type=int)
+    parser.add_argument("-wt", "--weight", help="填写体重公斤，支持小数点后一位，身高体重必须同时填写。", type=float)
+    args = parser.parse_args()
+
+    if args.blog:
+        # 添加孩子日记
+        insertBlog(first=args.first,language=args.language,cognitive=args.cognitive,blog=args.blog)
+    elif args.height and args.weight:
+        # 添加升高体重
+        insertHeightWeight(args.height,args.weight)
+
+    else:
+        print(parser.print_help())  # 默认打印帮助
+
+
+if __name__ == '__main__':
+    main()
